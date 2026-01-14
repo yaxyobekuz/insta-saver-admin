ebba90c0b5771ea769b6e7e8bb615d8b8c727006
yaxyobekuz <pubgn9642@gmail.com>
2026-01-14 10:12:25 +0500

    add CardComponent and Pagination component with responsive design and pagination functionality

---
+++ src/components/ui/card.component.jsx
@@ -0,0 +1,20 @@
+// Utils
+import { cn } from "@/utils/tailwind.utils";
+
+const CardComponent = ({ className = "", children, responsive = false }) => {
+  return (
+    <div
+      className={cn(
+        "bg-white",
+        responsive
+          ? "md:p-5 md:rounded-3xl md:border"
+          : "p-5 rounded-3xl border",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default CardComponent;
